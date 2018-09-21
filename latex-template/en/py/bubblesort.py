fa2c5e2e01170ceb999e2d0460217824dfa15d44
Janus Valberg-Madsen <janus@math.aau.dk>
2018-09-21 13:46:46 +0200

    Tentatively finished English template

---
+++ latex-template/en/py/bubblesort.py
@@ -0,0 +1,12 @@
+def bubblesort(a):
+    for i in range(len(a)-2):
+        for j in range(len(a)-i-1):
+            if a[j] > a[j+1]:
+                tmp = a[j]
+                a[j] = a[j+1]
+                a[j+1] = tmp
+
+
+ex = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+bubblesort(ex)
+print ex
